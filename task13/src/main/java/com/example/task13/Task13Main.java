c5903576135c54f907d6ef6414a0c7ba6b23c2f9
Plushika <plushika.core@gmail.com>
2026-09-11 19:57:53 +0500

    Валюженич Алексей. Задание 13

--- task13/src/main/java/com/example/task13/Task13Main.java
+++ task13/src/main/java/com/example/task13/Task13Main.java
@@ -4,16 +4,42 @@ public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
+
+        int[] arr = {9, 7, 0, 2323, 400};
+        arr = removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+
+        if (arr == null) {
+            return null;
+        }else {
+
+            int countNum = 0;
+            int countIter = 0;
+
+            for (int num : arr) {
+                if (num <= 1000) {
+                    countNum++;
+                }
+            }
+            int[] arrNew = new int[countNum];
+            sigh:
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] <= 1000) {
+                    for (int j = 0; j < countNum; j++) {
+                        if (arrNew[j] == 0 && countIter == j) {
+                            arrNew[j] = arr[i];
+                            countIter ++;
+                            continue sigh;
+                        }
+                    }
+                }
+            }
+            return arrNew;
+        }
     }
 
 }
