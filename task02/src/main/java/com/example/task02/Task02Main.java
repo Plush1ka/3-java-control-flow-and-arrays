c0f996a0856971e587847cee927e4d8ced6431d7
Plushika <plushika.core@gmail.com>
2026-09-11 17:24:35 +0500

    Валюженич Алексей. Задание 02

--- task02/src/main/java/com/example/task02/Task02Main.java
+++ task02/src/main/java/com/example/task02/Task02Main.java
@@ -4,12 +4,23 @@ public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getSeason(12));
-         */
+
+        System.out.println(getSeason(13));
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        if (monthNumber == 12 || (monthNumber >= 1 && monthNumber <= 2)) {
+            return "зима";
+        } else if (3 <= monthNumber && monthNumber <= 5){
+            return "весна";
+        } else if (6 <= monthNumber && monthNumber <= 8){
+            return "лето";
+        } else if (9 <= monthNumber && monthNumber <= 11){
+            return "осень";
+        } else {
+            return "Такого месяца нет";
+        }
     }
 }
