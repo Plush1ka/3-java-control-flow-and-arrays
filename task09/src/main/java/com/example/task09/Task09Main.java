1651f8fc080d5ea3f1b4653d2fdd6fefd226838b
Plushika <plushika.core@gmail.com>
2026-09-11 18:05:05 +0500

    Валюженич Алексей. Задание 09

--- task09/src/main/java/com/example/task09/Task09Main.java
+++ task09/src/main/java/com/example/task09/Task09Main.java
@@ -14,8 +14,15 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+
+        int minNum = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < minNum) {
+                minNum = arr[i];
+            }
+        }
+
+        return minNum;
     }
 
 }
