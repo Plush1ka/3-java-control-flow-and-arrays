13e371aec4841f948309371aa3ceb15e8f429211
Plushika <plushika.core@gmail.com>
2026-09-11 18:32:58 +0500

    Валюженич Алексей. Задание 10

--- task10/src/main/java/com/example/task10/Task10Main.java
+++ task10/src/main/java/com/example/task10/Task10Main.java
@@ -4,15 +4,24 @@ public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+
+        int[] arr = {1, 5, 7};
         System.out.println(numMin(arr));
-         */
+
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minNum = arr[0];
+        int numOfMin = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < minNum) {
+                minNum = arr[i];
+                numOfMin = i;
+            }
+            if (arr[i] == minNum) {
+                numOfMin = i;
+            }
+        }
+        return numOfMin;
     }
-
 }
